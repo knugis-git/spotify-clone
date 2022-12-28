4801abe74140187095ccd2a365b6467d45ba4578
elkCODE <121523529+elkCODE@users.noreply.github.com>
2022-12-28 01:49:38 +0100

    2.0
    + cookie clicker. you cant say no. im taking your virtual cookies MOHAHAHA
    
    - another kid went trough a loophole and is now free from my basement

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import navbar from "./components/navbar.vue";
 import front from "./components/front.vue";
+import cookie from "./components/cookie.vue";
+
+const cookieShow = ref(true);
+
+function hideCookie() {
+  cookieShow.value = false;
+}
 </script>
 
 <template>
+  <cookie @cookieShow="hideCookie" v-if="cookieShow" />
   <navbar />
   <front />
 </template>
---
+++ src/components/cookie.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const emit = defineEmits(["cookieShow"]);
+
+function cookieACC() {
+  emit("cookieShow");
+}
+</script>
+
+<template>
+  <div class="cookie-wrapper">
+    <div class="cookie-container">
+      <H1>Våra cookievillkor</H1>
+      <P
+        >I enlighet med europeiska lagar samt för att hjälpa din upplevelse på
+        våran webbsida samlar vi in kakor från denna webbsida.</P
+      >
+      <button @click="cookieACC">Acceptera cookies</button>
+    </div>
+  </div>
+</template>
